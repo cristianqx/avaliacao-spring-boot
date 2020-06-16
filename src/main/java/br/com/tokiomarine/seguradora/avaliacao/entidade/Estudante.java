7666e38f8588fedc239c015ea9b6817a6f2427f6
Cristian Baptistella <cristianqx@gmail.com>
2020-06-16 12:49:44 -0300

    getters e setters

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -31,4 +31,37 @@ public class Estudante implements Serializable {
 	
 	@Column(nullable = false)
 	private String telefone;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
 }
