630d13cef432b949fcc6c957b1a9d7640581067c
Cristian Baptistella <cristianqx@gmail.com>
2020-06-16 12:47:02 -0300

    configuracao entidades

--- src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
+++ src/main/java/br/com/tokiomarine/seguradora/avaliacao/entidade/Estudante.java
@@ -1,5 +1,34 @@
 package br.com.tokiomarine.seguradora.avaliacao.entidade;
 
-public class Estudante {
-	// TODO Implementar a entidade Estudante conforme solicitado
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Estudante implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	@NotEmpty
+	@NotNull(message = "Nome é obrigatório")
+	private String nome;
+	
+	@Column(nullable = false)
+	@NotEmpty
+	@NotNull(message = "E-mail é obrigatório")
+	private String email;
+	
+	@Column(nullable = false)
+	private String telefone;
 }
